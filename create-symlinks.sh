15242e45d43b7ab3a6876f38a40285e1219c623d
David Brewer <david.brewer@gmail.com>
2013-09-23 11:34:27 -0700

    Switching things to use new .vimrc.before format for spf13 configs.

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -3,3 +3,4 @@
 # Symlink the configuration files into their appropriate homes
 ln -s ~/.spf13-vim-local/.vimrc.local ~/.vimrc.local
 ln -s ~/.spf13-vim-local/.vimrc.bundles.local ~/.vimrc.bundles.local
+ln -s ~/.spf13-vim-local/.vimrc.before.local ~/.vimrc.before.local
